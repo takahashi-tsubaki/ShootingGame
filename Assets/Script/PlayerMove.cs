abdd0e489422b5afdedba7c18ee4e5f12fb0d9a9
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-07-05 16:38:01 +0900

    WASDでも移動が可能にした

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -62,19 +62,19 @@ public class PlayerMove : MonoBehaviour
     {
         Vector3 pos = transform.position;
 
-        if(Input.GetKey(KeyCode.UpArrow))
+        if(Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.W))
         {
             pos.z += speed;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             pos.z -= speed;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             pos.x -= speed;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             pos.x += speed;
         }
